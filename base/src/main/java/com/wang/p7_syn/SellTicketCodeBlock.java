fd50b8d8615376be0318c49fde66103364c74e0d
wangshenzhen0718 <57970628+wangshenzhen0718@users.noreply.github.com>
2023-05-09 11:47:23 +0800

    类锁和对象锁理解

--- base/src/main/java/com/wang/p7_syn/SellTicketCodeBlock.java
+++ base/src/main/java/com/wang/p7_syn/SellTicketCodeBlock.java
@@ -3,7 +3,7 @@ package com.wang.p7_syn;
 /**
  * @Author:wsz
  * @Date: 2023/1/4 22:53
- * @Description: 线程同步 synchronized加到代码块上
+ * @Description: 线程同步 synchronized加到代码块上 参考文章理解：https://blog.csdn.net/my_sunshine26/article/details/125769089
  * @Version: 1.0
  * @Since: 1.0
  */
@@ -27,6 +27,7 @@ class SellTicket02 implements Runnable{
 
     //静态方法锁加在SellTicket02.class
     public static void m1(){
+        //类锁：每个类只有一个Class对象，所以每个类只有一个类锁；类锁是加载类上的，而类信息是存在JVM方法区的，并且整个JVM只有一份，方法区又是所有线程共享的，所以类锁是所有线程共享的
         synchronized (SellTicket02.class){
             System.out.println("m1");
 
@@ -39,6 +40,7 @@ class SellTicket02 implements Runnable{
     //3.也可以加在代码块上这时锁还是在 this对象
     public void sell(){
 
+        //对象锁：每个实例都会有一个monitor对象，即Java对象的锁，类的对象可以有多个，所以每个对象有其独立的对象锁，互不干扰
         synchronized (obj/*this*/){
             if (ticketNum<=0){
                 System.out.println("售票结束。。。");
